dc0ff893fc99ca393d17317aea41fd38cc073071
Tom Simmonds <tomsimmonds11@gmail.com>
2022-04-06 20:30:48 +0100

    added custom motor config

--- libraries/AP_Motors/AP_Motors6DOF.cpp
+++ libraries/AP_Motors/AP_Motors6DOF.cpp
@@ -177,7 +177,12 @@ void AP_Motors6DOF::setup_motors(motor_frame_class frame_class, motor_frame_type
 
     case SUB_FRAME_CUSTOM:
         // Put your custom motor setup here
-        //break;
+            //                 Motor #              Roll Factor     Pitch Factor    Yaw Factor      Throttle Factor     Forward Factor      Lateral Factor  Testing Order
+        add_motor_raw_6dof(AP_MOTORS_MOT_1,     0,              0,              0,                  -1.0f,            0,               1.0f,       1);
+        add_motor_raw_6dof(AP_MOTORS_MOT_2,     0,              0,              0,                  -1.0f,           0,               -1.0f,        2);
+        add_motor_raw_6dof(AP_MOTORS_MOT_3,     0,              0,              1.0f,               0,                  1.0f,            0,              3);
+        add_motor_raw_6dof(AP_MOTORS_MOT_4,     0,              0,              -1.0f,              0,                  1.0f,            0,              4);
+        break;
 
     case SUB_FRAME_SIMPLEROV_3:
         _frame_class_string = "SIMPLEROV_3";
